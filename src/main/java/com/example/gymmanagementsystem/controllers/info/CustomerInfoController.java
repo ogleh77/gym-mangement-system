9e016ec66336d4d9c419327d7656e578a125bfee
ogleh77 <oglehmo@gmail.com>
2023-04-16 14:40:44 +0300

    Masha Allah customer info is done

--- src/main/java/com/example/gymmanagementsystem/controllers/info/CustomerInfoController.java
+++ src/main/java/com/example/gymmanagementsystem/controllers/info/CustomerInfoController.java
@@ -1,5 +1,6 @@
 package com.example.gymmanagementsystem.controllers.info;
 
+import com.example.gymmanagementsystem.controllers.main.PaymentController;
 import com.example.gymmanagementsystem.dao.main.PaymentService;
 import com.example.gymmanagementsystem.dao.service.GymService;
 import com.example.gymmanagementsystem.entities.main.Customers;
@@ -7,18 +8,22 @@ import com.example.gymmanagementsystem.entities.main.Payments;
 import com.example.gymmanagementsystem.entities.service.Gym;
 import com.example.gymmanagementsystem.helpers.CommonClass;
 import com.example.gymmanagementsystem.helpers.CustomException;
+import com.jfoenix.controls.JFXButton;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.time.LocalDate;
@@ -53,22 +58,16 @@ public class CustomerInfoController extends CommonClass implements Initializable
     private Label waist;
     @FXML
     private TableColumn<Payments, String> amountPaid;
-
     @FXML
     private TableColumn<Payments, String> discount;
-
     @FXML
     private TableColumn<Payments, LocalDate> expDate;
-
     @FXML
     private TableColumn<Payments, String> month;
-
     @FXML
     private TableColumn<Payments, String> paidBy;
-
     @FXML
     private TableColumn<Payments, String> paymentDate;
-
     @FXML
     private TableColumn<Payments, String> pend;
     @FXML
@@ -83,38 +82,41 @@ public class CustomerInfoController extends CommonClass implements Initializable
     private TableColumn<Payments, String> vipBox;
     @FXML
     private TableColumn<Payments, String> year;
+    @FXML
+    private JFXButton pendBtn;
     private ObservableList<Payments> payments;
     private final Gym currentGym;
-
     private final String pendStyle;
     private final String unPendStyle;
-
     private ButtonType ok;
-
     private ButtonType cancel;
 
     public CustomerInfoController() throws SQLException {
         this.currentGym = GymService.getGym();
-        this.pendStyle = "-fx-background-color: #afd6e3;-fx-text-fill: black;-fx-font-family:Verdana;" + "-fx-pref-width: 100;-fx-font-size: 15";
-        this.unPendStyle = "-fx-background-color: red;-fx-text-fill: white;-fx-font-family:Verdana;" + "-fx-pref-width: 100;-fx-font-size: 15";
+        this.pendStyle = "-fx-background-color: #328ca8;-fx-text-fill: white;-fx-font-family:Verdana;" + "-fx-font-size: 14";
+        this.unPendStyle = "-fx-background-color: red;-fx-text-fill: black;-fx-font-family:Verdana;" + "-fx-font-size: 14";
     }
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public void initialize(URL location, ResourceBundle resources) {
         Platform.runLater(this::initFields);
+        tableView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.isPending() && !newValue.isOnline()) {
+                pendBtn.setDisable(true);
+                pendBtn.setText("Haki(pend)");
+                pendBtn.setStyle(pendStyle);
+            } else if (newValue.isPending()) {
+                pendBtn.setText("Fur");
+                pendBtn.setStyle(unPendStyle);
+                pendBtn.setDisable(false);
+            } else {
+                pendBtn.setText("Haki(pend)");
+                pendBtn.setStyle(pendStyle);
+                pendBtn.setDisable(false);
+            }
+        });
     }
 
-    @FXML
-    void pendHandler() {
-    }
-
-    @FXML
-    public void editHandler() {
-    }
-
-    @FXML
-    public void deleteHandler() {
-    }
     @Override
     public void setCustomer(Customers customer) {
         super.setCustomer(customer);
@@ -122,11 +124,14 @@ public class CustomerInfoController extends CommonClass implements Initializable
             fullName.setText(customer.getFirstName() + " " + customer.getMiddleName() + " " + customer.getLastName());
             phone.setText(customer.getPhone());
             gander.setText(customer.getGander());
-            address.setText(customer.getAddress() == null ? " no address " : customer.getAddress());
+            address.setText(customer.getAddress() == null ? "no address " : customer.getAddress());
             shift.setText(customer.getShift());
-            weight.setText(customer.getWeight() + "");
+            weight.setText(customer.getWeight() + " kg");
             whoAdded.setText(customer.getWhoAdded());
-
+            waist.setText(customer.getWaist() + " cm");
+            hips.setText(customer.getHips() + " cm");
+            foreArm.setText(customer.getForeArm() + " cm");
+            chest.setText(customer.getChest() + " cm");
             try {
                 payments = PaymentService.fetchAllPayments(customer.getPhone());
             } catch (CustomException e) {
@@ -141,17 +146,67 @@ public class CustomerInfoController extends CommonClass implements Initializable
         }
     }
 
+    @FXML
+    void pendHandler() {
+        Payments selectedPayment = tableView.getSelectionModel().getSelectedItem();
+        try {
+            if (selectedPayment == null) {
+                throw new RuntimeException("No payment selected");
+            }
+            checkPayment(selectedPayment);
+        } catch (Exception e) {
+            infoAlert(e.getMessage());
+        }
+    }
 
+    @FXML
+    void editHandler() {
+        Payments selectedPayment = tableView.getSelectionModel().getSelectedItem();
+        try {
+            if (selectedPayment == null) {
+                throw new RuntimeException("No payment selected");
+            }
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/gymmanagementsystem/views/main-create/payments.fxml"));
+            Scene scene = new Scene(loader.load());
+            PaymentController controller = loader.getController();
+            controller.setUpdatePayment(selectedPayment);
+            controller.setCustomer(customer);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            errorMessage(e.getMessage());
+        }
+    }
 
-    //---------------------init fields-----------------–
+    @FXML
+    void deleteHandler() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/gymmanagementsystem/views/main-create/payments.fxml"));
+        Scene scene = new Scene(loader.load());
+        PaymentController controller = loader.getController();
+        controller.setCustomer(customer);
+        controller.checkPayment(customer);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //----------------------Helper methods----------------
+    private void checkPayment(Payments payment) throws SQLException {
+        if (payment.isPending()) {
+            unPayment(payment);
+        } else {
+            pendPayment(payment);
+        }
+        tableView.refresh();
+    }
 
     private void initFields() {
         if (payments.isEmpty()) {
             tableView.setPlaceholder(new Label("MACMIILKU PAYMENTS MALEH.."));
         } else {
             amountPaid.setCellValueFactory(payment -> new SimpleStringProperty("$" + payment.getValue().getAmountPaid()));
-            discount.setCellValueFactory(payment -> new SimpleStringProperty(payment.getValue().getDiscount() == 0 ?
-                    payment.getValue().getDiscount() + "" : "$" + payment.getValue().getDiscount()));
+            discount.setCellValueFactory(payment -> new SimpleStringProperty(payment.getValue().getDiscount() == 0 ? payment.getValue().getDiscount() + "" : "$" + payment.getValue().getDiscount()));
             expDate.setCellValueFactory(new PropertyValueFactory<>("expDate"));
             month.setCellValueFactory(new PropertyValueFactory<>("month"));
             paidBy.setCellValueFactory(new PropertyValueFactory<>("paidBy"));
@@ -159,70 +214,62 @@ public class CustomerInfoController extends CommonClass implements Initializable
             poxing.setCellValueFactory(payment -> new SimpleStringProperty(payment.getValue().isPoxing() ? "√" : "X"));
             running.setCellValueFactory(payment -> new SimpleStringProperty(payment.getValue().isOnline() ? "√" : "X"));
             pend.setCellValueFactory(payment -> new SimpleStringProperty(payment.getValue().isPending() ? "√" : "X"));
-
             daysRemind.setCellValueFactory(payment -> new SimpleStringProperty(payment.getValue().getDaysRemind()));
-
             vipBox.setCellValueFactory(payment -> new SimpleStringProperty(payment.getValue().getBox() != null ? "√" : "X"));
             year.setCellValueFactory(new PropertyValueFactory<>("year"));
             tableView.setItems(payments);
         }
-
-
     }
-
 
     private void pendPayment(Payments payment) throws SQLException {
         LocalDate exp = payment.getExpDate();
-        LocalDate pendingDate = LocalDate.now();
-
-        System.out.println("Exp date:- " + exp);
-        System.out.println("pend date:- " + pendingDate);
-        int daysRemind = Period.between(pendingDate, exp).getDays();
-        int month = Period.between(pendingDate, exp).getMonths();
-        System.out.println(month);
-        if (month > 0) {
-            daysRemind = 30;
-        }
-        System.out.println("Days rem " + daysRemind);
+        String daysRemain = getDaysRemind(exp);
         if (ok == null && cancel == null) {
             ok = new ButtonType("Haa", ButtonBar.ButtonData.OK_DONE);
             cancel = new ButtonType("Maya!", ButtonBar.ButtonData.CANCEL_CLOSE);
         }
-
-        Alert alert = new Alert(Alert.AlertType.INFORMATION, "Ma hubtaa inaad hakisto paymentkan oo ay u hadhay \n" + "Wakhtigiisa dhicitaanka " + daysRemind + " malmood", ok, cancel);
-
+        Alert alert = new Alert(Alert.AlertType.INFORMATION, "Ma hubtaa inaad hakisto paymentkan oo ay u hadhay \n" + "Wakhtigiisa dhicitaanka " + daysRemain, ok, cancel);
         Optional<ButtonType> result = alert.showAndWait();
-
         if (result.isPresent() && result.get() == ok) {
             PaymentService.holdPayment(payment, currentGym.getPendingDate());
             payment.setPending(true);
             payment.setOnline(false);
-            tableView.refresh();
+            pendBtn.setText("Fur");
+            pendBtn.setStyle(unPendStyle);
         } else {
             alert.close();
         }
     }
 
     private void unPayment(Payments payment) throws SQLException {
-
         if (ok == null && cancel == null) {
             ok = new ButtonType("Haa", ButtonBar.ButtonData.OK_DONE);
             cancel = new ButtonType("Maya!", ButtonBar.ButtonData.CANCEL_CLOSE);
         }
-
         Alert alert = new Alert(Alert.AlertType.INFORMATION, "Ma hubtaa inaad dib u furto paymentkan ", ok, cancel);
-
         Optional<ButtonType> result = alert.showAndWait();
-
         if (result.isPresent() && result.get() == ok) {
             PaymentService.unHoldPayment(payment);
             payment.setPending(false);
             payment.setOnline(true);
             payment.setExpDate(payment.getExpDate());
+            pendBtn.setText("Haki");
+            pendBtn.setStyle(pendStyle);
         } else {
             alert.close();
         }
     }
 
-
+    private String getDaysRemind(LocalDate expDate) {
+        Period period = Period.between(LocalDate.now(), expDate);
+        if (period.getYears() > 0) {
+            return period.getYears() + " sano " + (period.getMonths() > 0 ? "& " + period.getMonths() + " bilood" : "")
+                    + (period.getDays() > 0 ? " & " + period.getDays() + "malmood" : "");
+        } else if (period.getMonths() > 0) {
+            return period.getMonths() + " bilood " + (period.getDays() > 0 ? " & " + period.getDays() + "malmood" : "");
+        } else if (period.getDays() > 0) {
+            return period.getDays() == 1 ? "1 maalin" : period.getDays() + " malmood";
+        }
+        return "outdated";
+    }
 }
